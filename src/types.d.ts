6d26c8d410d317c0d4a428e272fc766b6113c841
Audra Stafursky <audrastafursky@Audras-MacBook-Pro.local>
2024-07-26 21:02:39 -0400

    combine name and degree fields for display purposes

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,11 @@ export interface Advocate {
     yearsOfExperience: number, 
     phoneNumber: string
 }
+
+export interface AdvocateDisplay {
+    advocate: string, 
+    city: string, 
+    specialties: string[], 
+    yearsOfExperience: number, 
+    phoneNumber: string 
+}
