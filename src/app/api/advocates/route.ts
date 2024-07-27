92115277d79102c2ddd1ab394cf4895ee0beeaa6
Audra Stafursky <audrastafursky@Audras-MacBook-Pro.local>
2024-07-27 16:18:30 -0400

    allow for pagination and set default pagination settings

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,13 +1,24 @@
 import { Advocate } from "@/types";
-import db from "../../../db";
+import db from "../../../db/index.js";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { NextRequest } from "next/server.js";
+import { asc } from 'drizzle-orm'
+//import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  
+  const pageSize: number = parseInt(searchParams.get('pageSize') as string) || 5
+  const page: number = parseInt(searchParams.get('page') as string) || 1
+  
+  const data = 
+    await db.select()
+    .from(advocates)
+    .orderBy(asc(advocates.id))
+    .limit(pageSize)
+    .offset((page - 1) * pageSize)
 
-  const data: Advocate[] = advocateData;
+  //const data: Advocate[] = advocateData;
 
   return Response.json({ data });
 }
