a8109072a1747fd59ae7db8606fb6e94e285d944
Audra Stafursky <audrastafursky@Audras-MacBook-Pro.local>
2024-07-28 20:49:48 -0400

    add loading state and refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,56 +7,61 @@ import { Advocate, AdvocateDisplay } from "@/types";
 import { useEffect, useState } from "react";
 import { ColDef } from 'ag-grid-community';
 
+const colDefs: ColDef<AdvocateDisplay>[] = [
+  { field: "advocate", filter: true, flex: 1 },
+  { field: "specialties", filter: true, flex: 2 },
+  { field: "yearsOfExperience", filter: true, flex: 1 },
+  { field: "city", filter: true, flex: 1 },
+  { field: "phoneNumber", filter: true, flex: 1 },
+]
+
+const pageSize = 5
+const totalRows = 15
+const pageSizeOptions = [5, 10, 15]
+
+function mapAdvocates(a: Advocate): AdvocateDisplay {
+  return {
+    advocate: a.firstName + " " + a.lastName + ", " + a.degree, 
+    city: a.city,  
+    phoneNumber: a.phoneNumber,
+    yearsOfExperience: a.yearsOfExperience, 
+    specialties: a.specialties,
+  }
+}
+
 export default function Home() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [advocates, setAdvocates] = useState<AdvocateDisplay[]>([]);
 
-  const colDefs = [
-    { field: "advocate", filter: true, flex: 1 },
-    { field: "specialties", filter: true, flex: 2 },
-    { field: "yearsOfExperience", filter: true, flex: 1 },
-    { field: "city", filter: true, flex: 1 },
-    { field: "phoneNumber", filter: true, flex: 1 },
-  ]
-
-  const pageSize = 5
-  const totalRows = 15
-
-  function mapAdvocates(a: Advocate): AdvocateDisplay {
-    return {
-      advocate: a.firstName + " " + a.lastName + ", " + a.degree, 
-      city: a.city,  
-      phoneNumber: a.phoneNumber,
-      yearsOfExperience: a.yearsOfExperience, 
-      specialties: a.specialties,
-    }
-  }
-
   useEffect(() => {
+    setIsLoading(true)
     try {
       fetch(`/api/advocates?pageSize=${totalRows}`).then((response) => {
         response.json().then((jsonResponse) => {
           setAdvocates(jsonResponse.data.map(mapAdvocates));
+          setIsLoading(false)
         });
       });
     } catch(e){
-      console.log("Failed to fetch advocates ", e)
+      console.error("Failed to fetch advocates ", e)
     }
     
   }, []);
 
   return (
-    <div
-      className="ag-theme-quartz" 
-      style={{ height: 500 }}
-    >
-      <AgGridReact
-          rowData={advocates}
-          columnDefs={colDefs as ColDef<AdvocateDisplay>[]}
-          pagination={true}
-          paginationPageSize={pageSize}
-          paginationPageSizeSelector={[5, 10, 15]}
-      />
-    </div>
+    <main>
+      { isLoading ? "One moment please..." : 
+      <div className="ag-theme-quartz h-[500px]">
+        <AgGridReact
+            rowData={advocates}
+            columnDefs={colDefs}
+            pagination={true}
+            paginationPageSize={pageSize}
+            paginationPageSizeSelector={pageSizeOptions}
+        />
+      </div>
+    }
+    </main>
   )
 
 }
